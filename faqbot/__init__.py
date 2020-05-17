09b2e758ee747267132e50310d71bf1fce2880cf
Vitaly Zaitsev <vitaly@easycoding.org>
2020-05-17 15:07:08 +0200

    Added FAQMessages class.

--- faqbot/__init__.py
+++ faqbot/__init__.py
@@ -249,30 +249,6 @@ class FAQBot:
         self.__schema = 1
         self.__logger = logging.getLogger(__name__)
         self.__settings = Settings(self.__schema)
-        self.__msgs = {
-            'fb_welcome': 'Please send me `/faq keyword` command and I will search through my database for you.',
-            'fb_notoken': 'No API token found. Cannot proceed. Forward API token using ENV option and try again!',
-            'fb_pmex': 'Failed to handle command in private chat with bot.',
-            'fb_faqexpt': 'An exception occurred when trying to execute database query.',
-            'fb_faqerr': 'Failed to execute your query. Please try again later!',
-            'fb_addlog': 'Admin {} ({}) has added a new keyword {} to the database.',
-            'fb_alsaddlog': 'Admin {} ({}) has added a new alias {} for the keyword {} to the database.',
-            'fb_remlog': 'Admin {} ({}) has removed keyword {} from the database.',
-            'fb_alsremlog': 'Admin {} ({}) has removed alias {} from the database.',
-            'fb_editlog': 'Admin {} ({}) has edited the keyword {} in the database.',
-            'fb_addmsg': 'The keyword *{}* was added to the database.',
-            'fb_alsaddmsg': 'A new alias *{}* for the keyword *{}* was added to the database.',
-            'fb_remmsg': 'The keyword *{}* and all its aliases were removed from the database.',
-            'fb_alsremmsg': 'The alias *{}* was removed from the database.',
-            'fb_editmsg': 'The keyword *{}* was updated in the database.',
-            'fb_crashed': 'Bot crashed. Scheduling restart in 30 seconds.',
-            'fb_mlreq': 'Failed to execute your query. Please read bot documentation!',
-            'fb_notfound': 'Cannot find anything matching the specified keyword in my database!',
-            'fb_listkw': 'Available keywords: {}.',
-            'fb_addexists': 'The *{}* keyword is already exists in our database. No actions will be performed.',
-            'fb_notexists': 'The *{}* keyword does not exists in our database. No actions will be performed.',
-            'fb_faqlink': 'You will find the answers for the most of questions in our unofficial FAQ: {}'
-        }
         if not self.__settings.tgkey:
             raise Exception(self.__msgs['fb_notoken'])
         self.__logger.setLevel(self.__settings.get_logging_level())
