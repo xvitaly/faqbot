c042aff442e108e0acf397ff2596f8e7b046c117
Vitaly Zaitsev <vitaly@easycoding.org>
2020-05-21 12:48:10 +0200

    Added factory class for messages.

---
+++ faqbot/modules/messages/factory.py
@@ -0,0 +1,31 @@
+# coding=utf-8
+
+# FAQ bot for Telegram Messenger
+# Copyright (c) 2019 - 2020 EasyCoding Team
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+class FAQMessagesFactory:
+    def add_language(self, lang, handler):
+        self.__handlers[lang] = handler
+
+    def get_language(self, lang):
+        producer = self.__handlers.get(lang)
+        if not producer:
+            raise ValueError(lang)
+        return producer()
+
+    def __init__(self):
+        self.__handlers = {}
